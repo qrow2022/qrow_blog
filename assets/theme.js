17d65adf8e0470798cbb5e6f6760a29cbbe41f0c
Qrow2022 <nathanielroot15@gmail.com>
2022-04-28 00:07:44 -0400

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1,18 +1,21 @@
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
 var headerLinks = document.querySelectorAll("[id='header-links']");
-for (var i = 0; i < headerLinks.length; i++) {
-    headerLinks[i].style.color = sessionStorage.getItem('cc');
+for (let x of headerLinks) {
+    headerLinks[x].style.color = sessionStorage.getItem('cc');
 }
-document.getElementsByClassName("site-title").style.color = sessionStorage.getItem('cc');
-function themeChange() {
-     if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
+document.getElementsByClassName("site-title") = sessionStorage.getItem('cc');
 
-            sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-            sessionStorage.setItem('cc', '#777');
+
+function themeChange() {
+    if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
+
+        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+        sessionStorage.setItem('cc', '#777');
         
      }
     else if (sessionStorage.getItem('bg') == null || undefined) {
+        
         sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
         sessionStorage.setItem('cc', '#777');
 
@@ -24,16 +27,12 @@ function themeChange() {
 
     }
 
-document.body.style.backgroundColor = sessionStorage.getItem('bg');
-document.body.style.color = sessionStorage.getItem('cc');
-//for (var i = 0; i < headerLinks.length; i++) {
-//    headerLinks[i].style.color = sessionStorage.getItem('cc');
-//}
+    document.body.style.backgroundColor = sessionStorage.getItem('bg');
+    document.body.style.color = sessionStorage.getItem('cc');
 
-for (let i of headerLinks) {
-    headerLinks[i].style.color = sessionStorage.getItem('cc');
-}
-
-document.getElementsByClassName("site-title").style.color = sessionStorage.getItem('cc');
+    for (let i of headerLinks) {
+        headerLinks[i].style.color = sessionStorage.getItem('cc');
+    }
+    document.getElementsByClassName("site-title").style.color = sessionStorage.getItem('cc');
 
 }
